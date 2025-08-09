dbfb3923f02f83875bc7a27c47384150abb2a274
Edison Andres M. <132613148+AndresM92@users.noreply.github.com>
2025-08-08 19:18:26 -0500

    Component Handle errors

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 
 require_once("Config/Config.php");
 
-
 $ruta = !empty($_GET['url']) ? $_GET['url'] : "Home/index";
 $array = explode("/", $ruta);
 $controller = $array[0];
@@ -12,9 +11,6 @@ $parametro = "";
 if (!empty($array[1])) {
     $metodo = $array[1];
     
-    /*if (!empty($array[1] != "")) {
-        $metodo = $array[1];
-    }*/
 }
 
 if (!empty($array[2])) {
@@ -24,12 +20,6 @@ if (!empty($array[2])) {
         }
         $parametro = trim($parametro, ",");
 
-    /*if (!empty($array[2] != "")) {
-        for ($i = 2; $i < count($array); $i++) {
-            $parametro .= $array[$i] . ",";
-        }
-        $parametro = trim($parametro, ",");
-    }*/
 }
 
 require_once ("Config/App/autoload.php");
@@ -40,8 +30,8 @@ if (file_exists($dirControllers)) {
     if (method_exists($controllerobj, $metodo)) {
         $controllerobj->$metodo($parametro);
     } else {
-        echo "No Existe el metodo";
+        header('location: '.base_url.'Errors');
     }
 } else {
-    echo "No Existe el controlador";
+    header('location: '.base_url.'Errors');
 }
